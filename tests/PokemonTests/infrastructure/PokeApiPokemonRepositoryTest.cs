0ab94e53adae43dbac2b12aa41146f4b7c4fc7b5
rcamison <85695417+rcamison@users.noreply.github.com>
2022-03-01 20:36:17 +0100

    Refactor Pokemon Repository Test

--- tests/PokemonTests/infrastructure/PokeApiPokemonRepositoryTest.cs
+++ tests/PokemonTests/infrastructure/PokeApiPokemonRepositoryTest.cs
@@ -4,6 +4,8 @@ using PokemonTests.Infrastructure.Dto;
 using RichardSzalay.MockHttp;
 using System.Net.Http;
 using System.Text.Json;
+using Moq;
+using PokemonTests.Domain;
 using Xunit;
 
 namespace PokemonTests.Infrastructure
@@ -20,7 +22,8 @@ namespace PokemonTests.Infrastructure
             var response = JsonSerializer.Serialize(pokeApiPokemonDto);
             mockHttp.When($"{pokemonUrl}/{pokeApiPokemonDto.Id}").Respond("application/json", response);
             var httpClient = new HttpClient(mockHttp);
-            var pokeApiPokemonRepository = new PokeApiPokemonRepository(httpClient);
+            var inMemoryPokemonFavouriteRepository = new Mock<InMemoryPokemonFavouriteRepository>();
+            var pokeApiPokemonRepository = new PokeApiPokemonRepository(httpClient, inMemoryPokemonFavouriteRepository.Object);
             //When
             var pokemon = pokeApiPokemonRepository.Find(new PokemonId(pokeApiPokemonDto.Id));
             //Then
@@ -36,11 +39,36 @@ namespace PokemonTests.Infrastructure
             var response = JsonSerializer.Serialize(pokeApiPokemonDto);
             mockHttp.When($"{pokemonUrl}/{pokeApiPokemonDto.Id}").Respond("application/json", response);
             var httpClient = new HttpClient(mockHttp);
-            var pokeApiPokemonRepository = new PokeApiPokemonRepository(httpClient);
+            var inMemoryPokemonFavouriteRepository = new Mock<InMemoryPokemonFavouriteRepository>();
+            var pokeApiPokemonRepository = new PokeApiPokemonRepository(httpClient, inMemoryPokemonFavouriteRepository.Object);
             //When
             var exists = pokeApiPokemonRepository.Exists(new PokemonId(pokeApiPokemonDto.Id));
             //Then
             Assert.True(exists);
         }
+        [Fact]
+        public void Should_Save_A_Pokemon()
+        {
+            //Given
+            string pokemonUrl = "https://pokeapi.co/api/v2/pokemon";
+            var mockHttp = new MockHttpMessageHandler();
+
+            var pokemonId = PokemonIdMother.Random();
+            var pokemon = PokemonMother.Random(pokemonId);
+            var pokeApiPokemonDto = PokeApiPokemonDtoMother.Random(pokemonId.Value);
+
+            var response = JsonSerializer.Serialize(pokeApiPokemonDto);
+            mockHttp.When($"{pokemonUrl}/{pokeApiPokemonDto.Id}").Respond("application/json", response);
+            var httpClient = new HttpClient(mockHttp);
+            var inMemoryPokemonFavouriteRepository = new Mock<InMemoryPokemonFavouriteRepository>();
+            var pokeApiPokemonRepository = new PokeApiPokemonRepository(httpClient, inMemoryPokemonFavouriteRepository.Object);
+
+            //When
+            pokeApiPokemonRepository.Save(pokemon);
+            //Then
+            var exists = pokeApiPokemonRepository.Exists(new PokemonId(pokeApiPokemonDto.Id));
+            Assert.True(exists);
+        }
+
     }
 }
