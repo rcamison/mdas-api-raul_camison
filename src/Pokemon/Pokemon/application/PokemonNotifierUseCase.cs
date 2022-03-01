24229225ebff09efeb3ae60ad0045da9798a279d
rcamison <85695417+rcamison@users.noreply.github.com>
2022-03-01 14:49:56 +0100

    Pokemon Notifier Use Case

---
+++ src/Pokemon/Pokemon/application/PokemonNotifierUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pokemon.Pokemon.Domain;
+using Pokemon.Pokemon.domain.services;
+
+namespace Pokemon.Pokemon.Application
+{
+    public class PokemonNotifierUseCase
+    {
+        private readonly PokemonCreator _pokemonCreator;
+
+        public PokemonNotifierUseCase(PokemonCreator pokemonCreator)
+        {
+            _pokemonCreator = pokemonCreator;
+        }
+
+        public void Execute(int pokemonIdparam)
+        {
+            var pokemonId = new PokemonId(pokemonIdparam);
+
+            _pokemonCreator.Execute(pokemonId);
+        }
+    }
+}
