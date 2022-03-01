4c675e929fb4a79115e9a9363a24b9692b72e74b
rcamison <85695417+rcamison@users.noreply.github.com>
2022-03-01 23:00:36 +0100

    Pokemon Aggregate test

---
+++ tests/PokemonTests/domain/PokemonShould.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace PokemonTests.Domain
+{
+    public class PokemonShould
+    {
+        [Fact]
+        public void Increase_PokemonFavouriteNumberOfTimes()
+        {
+            //Given
+
+            var pokemon = PokemonMother.Random();
+            var pokemonFavoriteNumberOfTimes = pokemon.PokemonFavouriteNumberOfTimes;
+
+            //When
+            pokemon.IncreasePokemonFavouriteNumberOfTimes();
+
+            //Then
+            Assert.True(pokemonFavoriteNumberOfTimes.Value + 1 == pokemon.PokemonFavouriteNumberOfTimes.Value);
+        }
+
+    }
+
+}
